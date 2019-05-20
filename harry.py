d979928016311c7083063548905e0dfcc159cbc2
Mike Veronie <mveronie@ufl.edu>
2019-05-20 16:53:13 -0400

    Return first <h1> and first <p> element as a test

--- harry.py
+++ harry.py
@@ -1,8 +1,20 @@
 import requests
+import re
 from pprint import pprint as pp
 from bs4 import BeautifulSoup as soup
 
 student_support_services_url = 'https://archive.catalog.ufl.edu/ugrad/1213/Pages/student-support-services.aspx'
 
-response = requests.get(student_support_services_url)
-pp(response.text, width=100)
+with requests.Session() as s:
+    response = s.get(student_support_services_url)
+    page_soup = soup(response.text, features="lxml")
+
+    regex_pattern = re.search
+
+    # print the first <h1> element
+    pp(page_soup.h1)
+    # print the first <p> element
+    pp(page_soup.p)
+
+    # Todo
+    # containers = page_soup.findAll('div', {"class": "s4" + \*\})
